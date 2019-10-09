e3dcdb820fae01fb8d25dfd32e15d14caa14b411
Brad Davis <bdavis@saintandreas.org>
2019-10-09 09:41:31 -0700

    Fix xr::Version

--- include/openxr/openxr.hpp
+++ include/openxr/openxr.hpp
@@ -185,29 +185,29 @@ class Version {
     //! @}
 
     //! Get the major component.
-    OPENXR_HPP_CONSTEXPR uint32_t major() const noexcept { return static_cast<uint32_t>(val_ >> 32); }
+    OPENXR_HPP_CONSTEXPR uint16_t major() const noexcept { return static_cast<uint16_t>(val_ >> 48); }
     //! Set the major component.
-    void major(uint32_t val) noexcept {
-        // blank the top 32 bits
-        val_ &= 0xffffffff;
-        val_ |= uint64_t(val) << 32;
+    void major(uint16_t val) noexcept {
+        // blank the top 16 bits
+        val_ &= 0x0000ffffffffffff;
+        val_ |= uint64_t(val) << 48;
     }
 
     //! Get the minor component.
-    OPENXR_HPP_CONSTEXPR uint16_t minor() const noexcept { return static_cast<uint16_t>((val_ >> 16) & 0xffff); }
+    OPENXR_HPP_CONSTEXPR uint16_t minor() const noexcept { return static_cast<uint16_t>((val_ >> 32) & 0xffff); }
     //! Set the minor component.
     void minor(uint16_t val) noexcept {
         // blank the correct 16 bits
-        val_ &= 0xffffffff0000ffff;
-        val_ |= uint64_t(val) << 16;
+        val_ &= 0xffff0000ffffffff;
+        val_ |= uint64_t(val) << 32;
     }
 
     //! Get the patch component.
-    OPENXR_HPP_CONSTEXPR uint16_t patch() const noexcept { return static_cast<uint16_t>(val_ & 0xffff); }
+    OPENXR_HPP_CONSTEXPR uint32_t patch() const noexcept { return static_cast<uint32_t>(val_ & 0xffffffff); }
     //! Set the patch component.
-    void patch(uint16_t val) noexcept {
-        // blank the least-significant 16 bits
-        val_ &= 0xffffffffffff0000;
+    void patch(uint32_t val) noexcept {
+        // blank the least-significant 32 bits
+        val_ &= 0xffffffff00000000;
         val_ |= val;
     }
 
